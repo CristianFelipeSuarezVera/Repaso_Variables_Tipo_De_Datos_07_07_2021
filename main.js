f1c87e59f5c217515be0f687afe01e7bac0aba8f
CristianFelipeSuarezVera <crisfelipesuarez@gmail.com>
2021-07-09 13:13:56 -0500

    Repaso de las variables y tipo de datos 07072021

---
+++ main.js
@@ -0,0 +1,16 @@
+
+nombreCompletoDelEstudiante = `Cristian Felipe Suárez Vera -`;
+edad = 32;
+soyUnEstudiante = true;
+profesion = `Administrador de Empresas -`;
+altura = 1.92;
+ciudadNacimiento = `Bucaramanga`;
+
+console.log(nombreCompletoDelEstudiante, typeof(nombreCompletoDelEstudiante));
+console.log(edad, typeof(edad));
+console.log(soyUnEstudiante, typeof(soyUnEstudiante));
+console.log(profesion, typeof(profesion));
+console.log(altura, typeof(altura));
+
+// alert()
+// document.write();
